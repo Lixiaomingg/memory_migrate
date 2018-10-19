aca23f26c1c781b8d5271714bf73ad9614c9af78
lihuhua <921159963@qq.com>
2018-10-19 12:55:27 +0800

    完成fdt和dentry的搜寻工作

--- code/dentry_migrate.c
+++ code/dentry_migrate.c
@@ -12,28 +12,38 @@
 #include <errno.h>
 #include <sys/mman.h>
 #include <inttypes.h>
+#include <stdbool.h>
 
-#define MAX_COUNT 256
+//128KB/512B
+#define MAX_FDT_COUNT 256
+
+//128KB/192B
+#define MAX_DENTRY_COUNT 682
 
 unsigned long tasks_offset = 0;
 unsigned long name_offset = 0;
+
+//迁移数量
 int migrate_count;
 
+//实际检测到fdt和dentry数量
 int fdt_count;
+int dentry_count;
 
 struct fdtable{
     addr_t addr;
     int next_fd;
 };
 
-void get_dentry_addr(vmi_instance_t vmi,addr_t addrs[MAX_COUNT]);
-status_t get_fdt_addr(vmi_instance_t vmi,struct fdtable addrs[MAX_COUNT]);
+status_t get_dentry_addr(vmi_instance_t vmi,struct fdtable fdt_addrs[MAX_FDT_COUNT],addr_t dentry_addrs[MAX_DENTRY_COUNT]);
+status_t get_fdt_addr(vmi_instance_t vmi,struct fdtable addrs[MAX_FDT_COUNT]);
+
 
 int main(int argc,char **argv){
     vmi_instance_t vmi;
     char *name = NULL;
-    struct fdtable fdt_addrs[MAX_COUNT]; 
-    addr_t dentry_addrs[MAX_COUNT];
+    struct fdtable fdt_addrs[MAX_FDT_COUNT]; 
+    addr_t dentry_addrs[MAX_DENTRY_COUNT];
     int i;
 
     if(argc != 2){
@@ -73,13 +83,13 @@ int main(int argc,char **argv){
         printf("failed to get fdt address\n");
         goto error_exit;
     }
+    printf("search task of fdt is finished ,the fdt_count is %d\n",fdt_count);
 
-    for(i = 0;i<fdt_count;i++){
-         printf("%lx %d \n",fdt_addrs[i].addr,fdt_addrs[i].next_fd);
+    if(VMI_FAILURE == get_dentry_addr(vmi,fdt_addrs,dentry_addrs)){
+        printf("failed to get dentry address\n");
+        goto error_exit;
     }
-    
-
-
+    printf("search task of dentry is finished ,the dentry_count is %d\n",dentry_count);    
 
 error_exit:
     vmi_resume_vm(vmi);
@@ -88,7 +98,7 @@ error_exit:
 }
 
 //从目标虚拟机内存中获取指定数量fdt数组首地址
-status_t get_fdt_addr(vmi_instance_t vmi,struct fdtable addrs[MAX_COUNT]){
+status_t get_fdt_addr(vmi_instance_t vmi,struct fdtable addrs[MAX_FDT_COUNT]){
     addr_t list_head;
     addr_t current_list_entry = list_head;
     addr_t next_list_entry;
@@ -147,7 +157,7 @@ update_current_entry:
 
         //增加跳出循环条件
         if(current_list_entry == list_head) break;
-        if(fdt_count == MAX_COUNT) break;
+        if(fdt_count == MAX_FDT_COUNT) break;
 
     }
     return VMI_SUCCESS;
@@ -156,7 +166,41 @@ update_current_entry:
 
 
 //从目标虚拟机内存中获取指定数量dentry结构体首地址
-void get_dentry_addr(vmi_instance_t vmi, addr_t addrs[MAX_COUNT]){
-    
+status_t get_dentry_addr(vmi_instance_t vmi, struct fdtable fdt_addrs[MAX_FDT_COUNT],  addr_t dentry_addrs[MAX_DENTRY_COUNT]){
+    int i,j,k;
+    addr_t file;
+    addr_t dentry;
+
+    dentry_count = 0;
+    bool flag = true;
+
+    for(i=0;i<fdt_count;i++){
+        for(j=0;j< fdt_addrs[i].next_fd;j++){
+            if(VMI_SUCCESS == vmi_read_addr_va(vmi,fdt_addrs[i].addr + 0x8*j,0,&file)){
+                if(file){
+                    //读取dentry地址
+                    if(VMI_SUCCESS == vmi_read_addr_va(vmi,file+0x18,0,&dentry)){
+                        //判断dentry有无重复
+                        for(k=0;k<dentry_count;k++){
+                            if(dentry == dentry_addrs[k]){
+                                flag = false;
+                                break;
+                            }
+                        }
+
+                        //无重复，则添加之dentry数组中
+                        dentry_addrs[dentry_count] = dentry;
+                        dentry_count++;
+                        
+                        //判断数组是否已满
+                        if(dentry_count == MAX_DENTRY_COUNT) return VMI_SUCCESS;
+                    }
+                }
+            }
+        }
+    }
+
+    return VMI_SUCCESS;
 
 }
+
